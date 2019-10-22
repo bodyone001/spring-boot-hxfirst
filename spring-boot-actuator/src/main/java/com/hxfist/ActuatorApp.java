9d312db14d1f0fc54c4b908abbba6468159ebe68
wengfei <wengfei2004@163.com>
2019-10-22 15:33:35 +0800

    actuactor

---
+++ spring-boot-actuator/src/main/java/com/hxfist/ActuatorApp.java
@@ -0,0 +1,16 @@
+package com.hxfist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ActuatorApp
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(ActuatorApp.class,args);
+    }
+}
